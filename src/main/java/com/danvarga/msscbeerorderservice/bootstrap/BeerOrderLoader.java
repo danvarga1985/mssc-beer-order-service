abb62965362222749f58b7b9da51bdb42498b752
danvarga1985 <danvarga1985@gmail.com>
2021-01-06 09:33:05 +0100

    Fixing BeerUpc values in BeerOrderLoader

--- src/main/java/com/danvarga/msscbeerorderservice/bootstrap/BeerOrderLoader.java
+++ src/main/java/com/danvarga/msscbeerorderservice/bootstrap/BeerOrderLoader.java
@@ -15,8 +15,8 @@ import java.util.UUID;
 public class BeerOrderLoader implements CommandLineRunner {
     public static final String TASTING_ROOM = "Tasting Room";
     public static final String BEER_1_UPC = "0631234200036";
-    public static final String BEER_2_UPC = "0631234300019";
-    public static final String BEER_3_UPC = "0083783375213";
+    public static final String BEER_2_UPC = "0631234500037";
+    public static final String BEER_3_UPC = "0631334260016";
 
     private final CustomerRepository customerRepository;
 
