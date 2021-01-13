9095edebaba371102480be9a886527cb421dbb59
danvarga1985 <danvarga1985@gmail.com>
2021-01-13 18:19:31 +0100

    Handling Validate Order Response

--- src/main/java/com/danvarga/msscbeerorderservice/services/BeerOrderManager.java
+++ src/main/java/com/danvarga/msscbeerorderservice/services/BeerOrderManager.java
@@ -2,7 +2,11 @@ package com.danvarga.msscbeerorderservice.services;
 
 import com.danvarga.msscbeerorderservice.domain.BeerOrder;
 
+import java.util.UUID;
+
 public interface BeerOrderManager {
 
     BeerOrder newBeerOrder(BeerOrder beerOrder);
+
+    void processValidationResult(UUID beerOrderId, Boolean isValid);
 }
