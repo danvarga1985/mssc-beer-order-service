da43cc72e7d3b0b841a36f5e18f02dc8b32db8c2
danvarga1985 <danvarga1985@gmail.com>
2021-01-13 15:16:04 +0100

    Adding Validate Order Request & Action

---
+++ src/main/java/com/danvarga/msscbeerorderservice/sm/actions/ValidateOrderAction.java
@@ -0,0 +1,41 @@
+package com.danvarga.msscbeerorderservice.sm.actions;
+
+import com.danvarga.brewery.model.events.ValidateOrderRequest;
+import com.danvarga.msscbeerorderservice.config.JmsConfig;
+import com.danvarga.msscbeerorderservice.domain.BeerOrder;
+import com.danvarga.msscbeerorderservice.domain.BeerOrderEventEnum;
+import com.danvarga.msscbeerorderservice.domain.BeerOrderStatusEnum;
+import com.danvarga.msscbeerorderservice.repositories.BeerOrderRepository;
+import com.danvarga.msscbeerorderservice.services.BeerOrderManagerImpl;
+import com.danvarga.msscbeerorderservice.web.mappers.BeerOrderMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ValidateOrderAction implements Action<BeerOrderStatusEnum, BeerOrderEventEnum> {
+
+    private final BeerOrderRepository beerOrderRepository;
+    private final BeerOrderMapper beerOrderMapper;
+    private final JmsTemplate jmsTemplate;
+
+    @Override
+    public void execute(StateContext<BeerOrderStatusEnum, BeerOrderEventEnum> context) {
+        String beerOrderId = (String) context.getMessage().getHeaders().get(BeerOrderManagerImpl.BEER_ORDER_ID_HEADER);
+
+        BeerOrder beerOrder = beerOrderRepository.findOneById(UUID.fromString(beerOrderId));
+
+        jmsTemplate.convertAndSend(JmsConfig.VALIDATE_ORDER_QUEUE, ValidateOrderRequest.builder()
+                .beerOrder(beerOrderMapper.beerOrderToDto(beerOrder))
+                .build());
+
+        log.debug("Sent Validation request to queue for order id " + beerOrderId);
+    }
+}
